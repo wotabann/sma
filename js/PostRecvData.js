9478da902c6f3e432a715b56cb052e579453d641
wotabann <wotabann@gmail.com>
2025-03-02 13:24:40 +0900

    Delete js/PostRecvData.js

--- js/PostRecvData.js
+++
@@ -1,7 +0,0 @@
-class PostRecvData {
-  constructor(jsonObject) {
-    this.Header      = jsonObject.Header;
-    this.ErrorString = jsonObject.ErrorString;
-    this.Payload     = jsonObject.Payload;
-  }
-}
